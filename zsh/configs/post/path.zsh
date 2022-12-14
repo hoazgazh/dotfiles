hoazgazh/dotfiles

commit 1ed0dd402103671c92012e656a3a73517a53e2f0

Fix sourcing of asdf.sh (#710)
Installing asdf via Homebrew now requires souring from a different path.

This same issue was recently fixed in thoughbot/laptop by @cpytel, and so I've copied the changes from this commit:
https://github.com/thoughtbot/laptop/commit/aa3a84e15e76af9bf3713c350354bc42e59749bc

zsh/configs/post/path.zsh
@@ -5,9 +5,7 @@ PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 if [ -f "$HOME/.asdf/asdf.sh" ]; then
   . "$HOME/.asdf/asdf.sh"
 elif which brew >/dev/null &&
-  BREW_DIR="$(dirname `which brew`)/.." &&
-  [ -f "$BREW_DIR/opt/asdf/asdf.sh" ]; then
-  . "$BREW_DIR/opt/asdf/asdf.sh"
+  . "$(brew --prefix asdf)/libexec/asdf.sh"
 fi
 
 # mkdir .git/safe in the root of repositories you trust

commit 45aac0b266942c91e3a7ff7ad60a97b703650b78

Fix ZSH syntax error (#711)
Fixes the syntax error introduced in #710.

zsh/configs/post/path.zsh
@@ -4,7 +4,7 @@ PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 # Try loading ASDF from the regular home dir location
 if [ -f "$HOME/.asdf/asdf.sh" ]; then
   . "$HOME/.asdf/asdf.sh"
-elif which brew >/dev/null &&
+elif which brew >/dev/null; then
   . "$(brew --prefix asdf)/libexec/asdf.sh"
 fi
 